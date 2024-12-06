3e154b5ff944e319ff50cb3756f93a5565c331f4
Dmitrii Pozdin <podmev@gmail.com>
2024-12-05 22:55:20 -0500

    AppConfig with templateCache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/Podmev/web-app-go/pkg/config"
 	"github.com/Podmev/web-app-go/pkg/handlers"
+	"github.com/Podmev/web-app-go/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,6 +13,13 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+	app.TemplateCache = tc
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
