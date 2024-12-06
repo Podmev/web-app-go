81f46e3920eabd165fdcaadd65867bc33b39a561
Dmitrii Pozdin <podmev@gmail.com>
2024-12-06 09:48:57 -0500

    using bmizerany/pat for routes

---
+++ cmd/web/routers.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/Podmev/web-app-go/pkg/config"
+	"github.com/Podmev/web-app-go/pkg/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	return mux
+}
