bbb81c891b00f731dad65e1823df1dd7f7831732
Dmitrii Pozdin <podmev@gmail.com>
2024-12-06 10:59:29 -0500

    adding session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,15 +5,30 @@ import (
 	"github.com/Podmev/web-app-go/pkg/config"
 	"github.com/Podmev/web-app-go/pkg/handlers"
 	"github.com/Podmev/web-app-go/pkg/render"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -6,3 +6,10 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/justinas/nosurf v1.1.1
 )
+
+require (
+	github.com/alexedwards/scs v1.4.1 // indirect
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
